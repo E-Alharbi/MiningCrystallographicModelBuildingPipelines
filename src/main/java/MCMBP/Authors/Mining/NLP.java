754ef4dfc3846a4060597d7f4ac18fe6592229fd
Emad Alharbi <emra500@york.ac.uk>
2020-07-12 20:26:42 +0100

    changed StanfordCoreNLP to load once when an object is created

--- src/main/java/MCMBP/Authors/Mining/NLP.java
+++ src/main/java/MCMBP/Authors/Mining/NLP.java
@@ -1,20 +1,52 @@
 package MCMBP.Authors.Mining;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
+
+import MCMBP.Utilities.Downloder;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreEntityMention;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
-public class NLP {
-
-	String TokenTypeCOUNTRY(String Txt) {
+public  class NLP {
+	static  StanfordCoreNLP pipeline;
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException{
 		
-	        Properties props = new Properties();
+		String json =  new Downloder().GetHttpRequste("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=32365117&resultType=core&format=json");
+		
+		LoadNLP();
+	
+		
+		
+		
+		
+		List<String> authors=new PMCMultiThreaded().ListofAffiliation(json);
+					
+				
+					for(int a=0 ;a < authors.size(); ++a) {
+
+					String val=TokenTypeCOUNTRY(authors.get(a).toString());
+					System.out.print(val);
+}
+		
+	}
+	static void LoadNLP() {
+		    Properties props = new Properties();
 	        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref,quote");
-	        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+	         pipeline = new StanfordCoreNLP(props);
+	         
+	       
+	}
+	static synchronized String TokenTypeCOUNTRY(String Txt) {
+		
+	     
 	        CoreDocument document = new CoreDocument(Txt);
 	        pipeline.annotate(document);
+	        
 	        // get confidences for entities
 	        String COUNTRY="";
 	        for (CoreEntityMention em : document.entityMentions()) {
@@ -25,6 +57,8 @@ public class NLP {
 	            	COUNTRY=em.text();
 	            
 	        }
+	       
+	       	
 	        return COUNTRY;
 	}
 }
