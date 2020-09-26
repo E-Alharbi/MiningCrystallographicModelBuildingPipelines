f39104e758b4061a8f4e51a61f559db5184bddbf
Emad Alharbi <emra500@york.ac.uk>
2020-09-26 20:52:57 +0100

    added back4app

--- src/main/java/MCMBP/Authors/Mining/Cluster.java
+++ src/main/java/MCMBP/Authors/Mining/Cluster.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
 
 import org.json.simple.parser.ParseException;
 
@@ -12,22 +13,22 @@ import MCMBP.Utilities.TxtFiles;
 
 public class Cluster {
 
-	public  void CreateJobs(String FilterBy , String Pipelines,String CrossrefEmail, String ElsevierToken , String PDBList , String JobParameters) throws IOException, ParseException, URISyntaxException, InterruptedException {
-		// TODO Auto-generated method stub
-
+	public  void CreateJobs(String FilterBy , String Pipelines,String CrossrefEmail, String ElsevierToken , String PDBList , String JobParameters , String  ApplicationIdBack4app ,  String APIKeyBack4app) throws IOException, ParseException, URISyntaxException, InterruptedException {
+		
 		String JarPath=Cluster.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().toString();
 		PMCMultiThreaded pmc = new PMCMultiThreaded();
 		pmc.PrepareDatasets(FilterBy, PDBList);
-		String Sbatch="";
 		
-		//int countSbatch=0;
+		
+		
 		for(String pdb : PMCMultiThreaded.PMCAndPDB.keySet()) {
 			if(!new File("papers/"+PMCMultiThreaded.PMCAndPDB.get(pdb).get("pubmedId")+".txt").exists()) {
-			String Script="#!/bin/bash\n" +JobParameters+"\n"; 
+			String Script="#!/bin/bash\n" +SplitJobParameters(JobParameters)+"\n"; 
 					
 			
-			Script+="\n java  -jar "+JarPath+" MiningAuthors FilterBy=\"[structureId:"+pdb+"]\" "+Pipelines+" CrossrefEmail="+CrossrefEmail+" ElsevierToken="+ElsevierToken+" ExtractingInformation=F";
+			Script+="\n java -Xmx3048m -jar "+JarPath+" MiningAuthors FilterBy=\"[structureId:"+pdb+"]\" Pipeline=\""+Pipelines+"\" CrossrefEmail="+CrossrefEmail+" ElsevierToken="+ElsevierToken+" ExtractingInformation=F "+" ApplicationIdBack4app="+ApplicationIdBack4app+" APIKeyBack4app="+APIKeyBack4app;
 			new TxtFiles().WriteStringToTxtFile(pdb+".sh",Script);
+			
 			if(GetNumberOfpendingJobs() < 9000) {
 				RunCmd("sbatch "+pdb+".sh");
 			}
@@ -37,19 +38,11 @@ public class Cluster {
 				}
 			}
 			
-			/*
-			Sbatch+="sbatch "+pdb+".sh \n";
-			if(Sbatch.split("\n").length>9000) {
-				new TxtFiles().WriteStringToTxtFile("Sbatch"+countSbatch+".sh",Sbatch);
-				countSbatch++;
-				
-				Sbatch="";
-			}
-			*/
+			
 			
 			}
 		}
-		//new TxtFiles().WriteStringToTxtFile("SbatchLast.sh",Sbatch);
+		
 		
 	}
 	
@@ -59,23 +52,70 @@ public class Cluster {
 	}
 	int GetNumberOfpendingJobs() throws IOException {
 		
-		BufferedReader stdInput = RunCmd("squeue -u "+GetLogname()+" -t PENDING | wc -l ");
+		BufferedReader stdInput = RunCmd("squeue -u "+GetLogname()+" -t PENDING");
 		String st="";
 		String Num="";
 		while ((st = stdInput.readLine()) != null) {
-			System.out.println(st);
-			Num+=st;
+			
+			Num+=st+"\n";
 		}
-		return Integer.valueOf(Num);
+		return Integer.valueOf(Num.split("\n").length);
 	}
 	
 	BufferedReader RunCmd(String cmd) throws IOException {
-		System.out.println(cmd);
+		
 		Runtime rt = Runtime.getRuntime();
 		Process pr = rt.exec(cmd);
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 return stdInput;
 	}
 	
+	String  SplitJobParameters(String Parameters){
+		String Pram="";
+		String Val="";
+		boolean IsParm=false;
+		boolean IsVal=false;
+		
+		LinkedHashMap<String,String> JobParameters=new LinkedHashMap<String,String>();  
+		
+		for(int i=0 ; i < Parameters.length();++i) {
+			if(Parameters.charAt(i)==']') {
+				
+				JobParameters.put(Pram, Val);
+				Pram="";
+				Val="";
+				IsParm=false;
+				IsVal=false;
+				
+			}
+			if(IsVal==true) {
+				Val+=Parameters.charAt(i);
+			}
+	        if(Parameters.charAt(i)=='#') {
+				
+				IsParm=false;
+				IsVal=true;
+			}
+			if(IsParm==true) {
+				Pram+=Parameters.charAt(i);
+			}
+			if(Parameters.charAt(i)=='[') {
+				Pram="";
+				IsParm=true;
+			}
+			
+			
+		}
+		
+		String ScriptHeader="";
+		for(String Parm : JobParameters.keySet()) {
+			if(JobParameters.get(Parm).trim().length()!=0)
+			ScriptHeader+="#SBATCH "	+Parm+"="+ JobParameters.get(Parm) + "\n";
+			else
+				ScriptHeader+=Parm+"\n";
+		}
+		
+		return ScriptHeader;
+	}
 
 }
