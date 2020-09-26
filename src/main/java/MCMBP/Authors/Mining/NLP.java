555c0ca09a0014c5821050cf6eebaad18722d600
Emad Alharbi <emra500@york.ac.uk>
2020-09-26 20:55:30 +0100

    removed unused property

--- src/main/java/MCMBP/Authors/Mining/NLP.java
+++ src/main/java/MCMBP/Authors/Mining/NLP.java
@@ -1,61 +1,71 @@
 package MCMBP.Authors.Mining;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Properties;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import MCMBP.Utilities.Downloder;
+import MCMBP.Utilities.GeoInfo;
+import MCMBP.Utilities.TxtFiles;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreEntityMention;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public  class NLP {
 	static  StanfordCoreNLP pipeline;
-	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException{
-		
-		String json =  new Downloder().GetHttpRequste("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=32365117&resultType=core&format=json");
-		
-		LoadNLP();
-	
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException, XPathExpressionException, ParserConfigurationException, SAXException{
 		
 		
-		
-		
-		List<String> authors=new PMCMultiThreaded().ListofAffiliation(json);
 					
-				
-					for(int a=0 ;a < authors.size(); ++a) {
 
-					String val=TokenTypeCOUNTRY(authors.get(a).toString());
-					System.out.print(val);
-}
 		
 	}
 	static void LoadNLP() {
 		    Properties props = new Properties();
-	        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref,quote");
+	   //     props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref,quote");
+	        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner");
 
 	         pipeline = new StanfordCoreNLP(props);
 	         
 	       
 	}
-	static synchronized String TokenTypeCOUNTRY(String Txt) {
+	static synchronized String TokenTypeCOUNTRY(String Txt , String ApplicationIdBack4app , String APIKeyBack4app) throws UnsupportedEncodingException, MalformedURLException {
 		
-	     
+	    
 	        CoreDocument document = new CoreDocument(Txt);
 	        pipeline.annotate(document);
 	        
 	        // get confidences for entities
 	        String COUNTRY="";
+	       
 	        for (CoreEntityMention em : document.entityMentions()) {
-	           // System.out.println(em.text() + "\t" + em.entityTypeConfidences());
-	          // System.out.println(em.text() + "\t" + em.entityType());
-	           // System.out.println(em.text() + "\t" + em.entity());
+	          
+	        	
 	            if(em.entityType().equals("COUNTRY"))
 	            	COUNTRY=em.text();
 	            
+	        }
+	        
+	        if(COUNTRY.trim().length()==0) {
+	        for (CoreEntityMention em : document.entityMentions()) {
+	        	
+	        	if(em.entityType().equals("CITY")||em.entityType().equals("STATE_OR_PROVINCE") || em.entityType().equals("LOCATION")) {
+	        		
+	        	COUNTRY=new GeoInfo().country(em.text(), ApplicationIdBack4app ,  APIKeyBack4app);
+	        }
+	             if(COUNTRY!=null && COUNTRY.trim().length()!=0) // if we get the country, we did not need to continue 
+	            	break;
+	        }
 	        }
 	       
 	       	
