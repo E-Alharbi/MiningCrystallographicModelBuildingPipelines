09fd83fc73017742e2724a9f876b9ce34286cef7
Emad Alharbi <emra500@york.ac.uk>
2020-10-17 09:00:52 +0100

    formated the code

--- src/main/java/MCMBP/Resources/PDBe.java
+++ src/main/java/MCMBP/Resources/PDBe.java
@@ -5,31 +5,22 @@ import java.util.HashMap;
 
 import org.json.simple.parser.ParseException;
 
-import MCMBP.Utilities.CSVReader;
 import MCMBP.Utilities.Downloder;
 import MCMBP.Utilities.JSONReader;
 
 public class PDBe {
 
-	public static void main(String[] args) throws IOException, ParseException {
-		// TODO Auto-generated method stub
-		
-		
+	public HashMap<String, HashMap<String, String>> UpdateDOI(HashMap<String, HashMap<String, String>> pdbbank,
+			String PDB) throws IOException, ParseException {
 
-	}
+		String pdbe = new Downloder().GetHttpRequste("https://www.ebi.ac.uk/pdbe/api/pdb/entry/publications/" + PDB);
+		if (pdbe.trim().length() != 0) {
+			HashMap<String, String> PDBe = new JSONReader().JSONToHashMap(pdbe);
 
-	
-	public HashMap<String, HashMap<String, String>> UpdateDOI(HashMap<String, HashMap<String, String>> pdbbank, String PDB) throws IOException, ParseException {
-		
-		String pdbe=new Downloder().GetHttpRequste("https://www.ebi.ac.uk/pdbe/api/pdb/entry/publications/"+PDB);
-		if(pdbe.trim().length()!=0) {
-		HashMap<String, String> PDBe= new JSONReader().JSONToHashMap(pdbe);
-		
-		//for(String pdb : pdbbank.keySet()) {
-			String pubmed_id=pdbbank.get(PDB).get("pubmedId");
-			if(pdbbank.get(PDB).get("doi").trim().length()==0)
-			pdbbank.get(PDB).put("doi", PDBe.get(pubmed_id));
-		//}
+			String pubmed_id = pdbbank.get(PDB).get("pubmedId");
+			if (pdbbank.get(PDB).get("doi").trim().length() == 0)
+				pdbbank.get(PDB).put("doi", PDBe.get(pubmed_id));
+
 		}
 		return pdbbank;
 	}
