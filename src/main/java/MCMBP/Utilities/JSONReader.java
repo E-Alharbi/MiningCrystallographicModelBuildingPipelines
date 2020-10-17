f1124350457499413822f3398b423b1ecc5c7749
Emad Alharbi <emra500@york.ac.uk>
2020-10-17 08:58:56 +0100

    formated the code

--- src/main/java/MCMBP/Utilities/JSONReader.java
+++ src/main/java/MCMBP/Utilities/JSONReader.java
@@ -1,51 +1,31 @@
 package MCMBP.Utilities;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.Vector;
 
-import org.json.simple.JSONObject;
-/*
- * Reading JSON into HashMap
- */
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class JSONReader {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-		// TODO Auto-generated method stub
-
-		new JSONReader().JSONToHashMap("");
-
-	}
-
 	public HashMap<String, String> JSONToHashMap(String JSONPath)
 			throws FileNotFoundException, IOException, ParseException {
-		
-		Vector<String> JsonRegx=new Vector<String>();
+
+		Vector<String> JsonRegx = new Vector<String>();
 		JsonRegx.add("$..pubmed_id");
 		JsonRegx.add("$..doi");
-		HashMap<String, String> pubmedidAnddoi=new HashMap<String, String>();
-		
+		HashMap<String, String> pubmedidAnddoi = new HashMap<String, String>();
+
 		List<String> pubmed_id = com.jayway.jsonpath.JsonPath.parse(JSONPath).read(JsonRegx.get(0));
 		List<String> doi = com.jayway.jsonpath.JsonPath.parse(JSONPath).read(JsonRegx.get(1));
 
-		for(int i=0 ; i < pubmed_id.size();++i) {
+		for (int i = 0; i < pubmed_id.size(); ++i) {
 			pubmedidAnddoi.put(pubmed_id.get(i), doi.get(i));
 		}
-		
-		
+
 		return pubmedidAnddoi;
-		
+
 	}
 }
