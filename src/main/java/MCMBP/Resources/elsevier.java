87d32673bd9501409b20872ac10a0e46c07e385a
Emad Alharbi <emra500@york.ac.uk>
2020-10-17 08:58:48 +0100

    formated the code

--- src/main/java/MCMBP/Resources/elsevier.java
+++ src/main/java/MCMBP/Resources/elsevier.java
@@ -6,9 +6,9 @@ import MCMBP.Utilities.Downloder;
 
 public class elsevier {
 
-	
 	public String Get(String ElsevierToken, String doi) throws IOException {
-		String PMC=new Downloder().GetHttpRequste("https://api.elsevier.com/content/article/doi/"+doi+"?APIKey="+ElsevierToken);
-return PMC;
+		String PMC = new Downloder()
+				.GetHttpRequste("https://api.elsevier.com/content/article/doi/" + doi + "?APIKey=" + ElsevierToken);
+		return PMC;
 	}
 }
