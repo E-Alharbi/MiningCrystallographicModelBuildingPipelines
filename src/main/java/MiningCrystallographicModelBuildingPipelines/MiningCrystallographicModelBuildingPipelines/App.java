63e1893c9aabe88e7bcc9c47e037f01e9348cf74
Emad Alharbi <emra500@york.ac.uk>
2020-10-17 08:58:03 +0100

    formated the code

--- src/main/java/MiningCrystallographicModelBuildingPipelines/MiningCrystallographicModelBuildingPipelines/App.java
+++ src/main/java/MiningCrystallographicModelBuildingPipelines/MiningCrystallographicModelBuildingPipelines/App.java
@@ -4,10 +4,8 @@ package MiningCrystallographicModelBuildingPipelines.MiningCrystallographicModel
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
